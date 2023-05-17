716846b7f8bd2ff11d2d296d8657fabb96b14b87
root <root@ip-10-0-21-44.us-east-2.compute.internal>
2023-05-17 17:50:39 +0000

    Shyd

--- main.tf
+++ main.tf
@@ -1,9 +1,12 @@
 provider "aws" {
   region = "us-east-2"
 }
+resource "aws_instance" "" {
+  ami           = var.ami
+  instance_type = var.instance_type
 
-resource "aws_instance" "example" {
-  ami           = "ami-083eed19fc801d7a4"
-  instance_type = "t2.nano"
-  subnet_id = "subnet-0050a67ec10dd04ce"
+  network_interface {
+    network_interface_id = var.network_interface_id
+    device_index         = 0
+  }
 }
--- variables.tf
+++ variables.tf
@@ -1,6 +1,22 @@
 variable "access_key" {
   type = string
 }
+
 variable "secret_key" {
   type = string
+}
+
+variable "network_interface_id" {
+  type    = string
+  default = "network_id_from_aws"
+}
+
+variable "ami" {
+  type    = string
+  default = "ami-005e54dee72cc1d00"
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t2.micro"
 }
