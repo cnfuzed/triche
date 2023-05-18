3acc62eb8d3f200169c645d9ec9460a72fe6491d
root <root@ip-10-0-21-44.us-east-2.compute.internal>
2023-05-18 15:48:51 +0000

    Update

--- main.tf
+++ main.tf
@@ -4,101 +4,187 @@ provider "aws" {
 }
 
 # Create VPC
-resource "aws_vpc" "automate" {
+resource "aws_vpc" "automate_jenkins" {
   cidr_block = "192.168.0.0/16"
 
   tags = {
-    Name = "automate-vpc"
+    Name = "Automate-Jenkins-vpc"
   }
 }
 
 # Create public subnet 1
 resource "aws_subnet" "public_subnet_1" {
-  vpc_id            = aws_vpc.automate.id
+  vpc_id            = aws_vpc.automate_jenkins.id
   cidr_block        = "192.168.1.0/24"
   availability_zone = "us-east-2a"
 
   tags = {
-    Name = "automate-public-subnet-1"
+    Name = "Automate-Jenkins-public-subnet-1"
   }
 }
 
 # Create public subnet 2
 resource "aws_subnet" "public_subnet_2" {
-  vpc_id            = aws_vpc.automate.id
+  vpc_id            = aws_vpc.automate_jenkins.id
   cidr_block        = "192.168.2.0/24"
   availability_zone = "us-east-2b"
 
   tags = {
-    Name = "automate-public-subnet-2"
+    Name = "Automate-Jenkins-public-subnet-2"
   }
 }
 
 # Create private subnet 1
 resource "aws_subnet" "private_subnet_1" {
-  vpc_id            = aws_vpc.automate.id
+  vpc_id            = aws_vpc.automate_jenkins.id
   cidr_block        = "192.168.3.0/24"
   availability_zone = "us-east-2a"
 
   tags = {
-    Name = "automate-private-subnet-1"
+    Name = "Automate-Jenkins-private-subnet-1"
   }
 }
 
 # Create private subnet 2
 resource "aws_subnet" "private_subnet_2" {
-  vpc_id            = aws_vpc.automate.id
+  vpc_id            = aws_vpc.automate_jenkins.id
   cidr_block        = "192.168.4.0/24"
   availability_zone = "us-east-2b"
 
   tags = {
-    Name = "automate-private-subnet-2"
+    Name = "Automate-Jenkins-private-subnet-2"
   }
 }
 
-# Create NAT gateway
-resource "aws_nat_gateway" "automate" {
-  allocation_id = aws_eip.nat_eip.id
-  subnet_id     = aws_subnet.public_subnet_1.id
-}
-
-# Create Elastic IP for NAT gateway
-resource "aws_eip" "nat_eip" {
-  vpc = true
+# Create internet gateway
+resource "aws_internet_gateway" "automate_jenkins" {
+  vpc_id = aws_vpc.automate_jenkins.id
 
   tags = {
-    Name = "automate-nat-eip"
+    Name = "Automate-Jenkins-internet-gateway"
   }
 }
 
-# Launch Amazon Linux 2 instance
-resource "aws_instance" "automate" {
+# Create route table for public subnets
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.automate_jenkins.id
+
+  tags = {
+    Name = "Automate-Jenkins-public-route-table"
+  }
+}
+
+# Associate route table with public subnet 1
+resource "aws_route_table_association" "public_subnet_1_association" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+# Associate route table with public subnet 2
+resource "aws_route_table_association" "public_subnet_2_association" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+# Create default route to the internet gateway in the public route table
+resource "aws_route" "public_route" {
+  route_table_id         = aws_route_table.public_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.automate_jenkins.id
+}
+
+# Create route table for private subnets
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.automate_jenkins.id
+
+  tags = {
+    Name = "Automate-Jenkins-private-route-table"
+  }
+}
+
+# Associate route table with private subnet 1
+resource "aws_route_table_association" "private_subnet_1_association" {
+  subnet_id      = aws_subnet.private_subnet_1.id
+  route_table_id = aws_route_table.private_route_table.id
+}
+
+# Associate route table with private subnet 2
+resource "aws_route_table_association" "private_subnet_2_association" {
+  subnet_id      = aws_subnet.private_subnet_2.id
+  route_table_id = aws_route_table.private_route_table.id
+}
+
+# Create Amazon Linux 2 instance
+resource "aws_instance" "automate_jenkins" {
   ami           = "ami-0c94855ba95c71c99" # Replace with the desired Amazon Linux 2 AMI ID
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.public_subnet_1.id
 
   tags = {
-    Name = "automate-instance"
+    Name = "Automate-Jenkins-instance"
   }
 }
 
-# Associate public EIP with the instance
-resource "aws_eip_association" "automate" {
-  instance_id   = aws_instance.automate.id
-  allocation_id = aws_eip.instance_eip.id
+# Create security group for the instance allowing ports 22, 80, and 443
+resource "aws_security_group" "automate_jenkins_sg" {
+  vpc_id = aws_vpc.automate_jenkins.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Automate-Jenkins-security-group"
+  }
 }
 
-# Create Elastic IP for the instance
-resource "aws_eip" "instance_eip" {
+# Associate security group with the instance
+resource "aws_instance_security_group" "automate_jenkins_sg_association" {
+  instance_id       = aws_instance.automate_jenkins.id
+  security_group_id = aws_security_group.automate_jenkins_sg.id
+}
+
+# Allocate Elastic IP
+resource "aws_eip" "automate_jenkins_eip" {
   vpc = true
 
   tags = {
-    Name = "automate-instance-eip"
+    Name = "Automate-Jenkins-eip"
   }
 }
 
-# Output the EIP assigned to the instance
+# Associate Elastic IP with the instance
+resource "aws_eip_association" "automate_jenkins_eip_association" {
+  instance_id   = aws_instance.automate_jenkins.id
+  allocation_id = aws_eip.automate_jenkins_eip.id
+}
+
+# Output the Elastic IP assigned to the instance
 output "instance_eip" {
-  value = aws_eip.instance_eip.public_ip
+  value = aws_eip.automate_jenkins_eip.public_ip
 }
 
