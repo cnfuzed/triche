ddcd269007f96f04df6fa92853c7c7a2422d84bb
root <root@ip-10-0-21-44.us-east-2.compute.internal>
2023-05-18 15:15:20 +0000

    Update

--- main.tf
+++ main.tf
@@ -2,24 +2,30 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_vpc" "custom_vpc" {
-  cidr_block = "10.0.0.0/16"
+resource "aws_vpc" "JenkinsVCP" {
+  cidr_block = "192.0.0.0/16"
 }
 
-resource "aws_subnet" "custom_subnet" {
-  vpc_id            = aws_vpc.custom_vpc.id
-  cidr_block        = "10.0.0.0/24"
+resource "aws_subnet" "JenkinsVCP_subnet" {
+  vpc_id            = aws_vpc.JenkinsVCP_vpc.id
+  cidr_block        = "192.0.0.0/24"
   availability_zone = "us-east-2a"
 }
 
-resource "aws_security_group" "custom_sg" {
+resource "aws_security_group" "Jenkins_sg" {
   name        = "my-ec2-sg"
   description = "Security group for EC2 instance"
-  vpc_id      = aws_vpc.custom_vpc.id
+  vpc_id      = aws_vpc.JenkinsVCP_vpc.id
 
   ingress {
     from_port   = 22
     to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -31,7 +37,9 @@ resource "aws_security_group" "custom_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-
+resource "aws_internet_gateway" "Jenkin-NAT" {
+  vpc_id = aws_vpc.Jenkins-NAT.id
+}
 resource "aws_instance" "Jenkins" {
   ami                    = "ami-083eed19fc801d7a4"
   instance_type          = "t2.micro"
@@ -52,6 +60,9 @@ resource "aws_eip_association" "eip_assoc" {
   allocation_id = aws_eip.myeip.id
 }
 
+resource "aws_internet_gateway" "Jenkins" {
+  vpc_id = aws_vpc.Jenkins.id
+}
 
 output "instance_public_ip" {
   value       = aws_instance.Jenkins.public_ip
