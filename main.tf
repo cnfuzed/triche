556014e3532a00e86f8cce5855238458a23e88f3
root <root@ip-10-0-21-44.us-east-2.compute.internal>
2023-05-18 04:32:30 +0000

    Role back

--- main.tf
+++ main.tf
@@ -1,11 +1,8 @@
 provider "aws" {
-    region = "us-east-1"
+  region = "us-west-2"
 }
 
-resource "aws_instance" "ec2_instance" {
-    ami = "${var.ami_id}"
-    count = "${var.number_of_instances}"
-    subnet_id = "${var.subnet_id}"
-    instance_type = "${var.instance_type}"
-    key_name = "${var.ami_key_pair_name}"
+resource "aws_instance" "example" {
+  ami           = "ami-083eed19fc801d7a4"
+  instance_type = "t2.nano"
 }
--- variables.tf
+++ variables.tf
@@ -5,31 +5,3 @@ variable "access_key" {
 variable "secret_key" {
   type = string
 }
-variable "instance_name" {
-        description = "Name of the instance to be created"
-        default = "awsbuilder-demo"
-}
-
-variable "instance_type" {
-        default = "t2.micro"
-}
-
-variable "subnet_id" {
-        description = "The VPC subnet the instance(s) will be created in"
-        default = "subnet-0050a67ec10dd04ce"
-}
-
-variable "ami_id" {
-        description = "The AMI to use"
-        default = "ami-09d56f8956ab235b3"
-}
-
-variable "number_of_instances" {
-        description = "number of instances to be created"
-        default = 1
-}
-
-
-variable "ami_key_pair_name" {
-        default = "lab6"
-}
